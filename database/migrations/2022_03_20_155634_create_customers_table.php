58c3a7dc25607a71bce2b89a40f15633da9a373f
Maria-Papa <85803896+Maria-Papa@users.noreply.github.com>
2022-03-20 18:15:35 +0200

    Add missing migration

---
+++ database/migrations/2022_03_20_155634_create_customers_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('company_id')->constrained();
+            $table->string('name');
+            $table->string('surname');
+            $table->integer('telephone')->unique();
+            $table->text('remarks');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('customers', function (Blueprint $table) {
+            $table->dropForeign(['company_id']);
+        });
+        Schema::dropIfExists('customers');
+    }
+}
