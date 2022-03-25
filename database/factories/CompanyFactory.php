af4130456b1da0d0405c20499a15158cbcd998cf
dkpapalekas <dkpapalekas@gmail.com>
2022-03-25 16:38:40 +0200

    added seeder with factory for Companies Table

---
+++ database/factories/CompanyFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CompanyFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->Company(10),
+            'address' => $this->faker->address(10),
+            'city' => $this->faker->city(10),
+            'profession' => $this->faker->bs(10),
+            'vat' => $this->faker->randomNumber,
+            'irs' => $this->faker->text(10)
+        ];
+    }
+}
