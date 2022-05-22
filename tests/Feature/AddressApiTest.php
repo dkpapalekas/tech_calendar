55fb5f8390b0200cc6ad954a86b540a713456e3e
dkpapalekas <dkpapalekas@gmail.com>
2022-05-23 01:26:03 +0300

    address API full test completed

--- tests/Feature/AddressApiTest.php
+++ tests/Feature/AddressApiTest.php
@@ -31,4 +31,71 @@ class AddressApiTest extends TestCase
             ->post('http://localhost:8000/api/v1/address',$form_data)
             ->assertStatus(201);
     }
+
+    public function test_get_address()
+    {   
+        $user = User::factory()->create();
+
+        $req = $this->actingAs($user)
+            ->get('http://localhost:8000/api/v1/addresses/4')
+            ->assertStatus(200)
+            ->dump();
+    }
+
+    // public function test_index_address()
+    // {   
+    //     $user = User::factory()->create();
+
+    //     $req = $this->actingAs($user)
+    //         ->get('http://localhost:8000/api/v1/addresses')
+    //         ->assertStatus(200)
+    //         ->dump();
+    // }
+
+    public function test_update_address()
+    {   
+        $user = User::factory()->create();
+
+        $form_data = [
+            'customer_id' => 5,
+            'name' => 'newwwww',
+            'number' => 6,
+            'city' => 'Korinthos',
+            'floor' => 5,
+            'remarks' => 'remark',
+        ];
+
+        $req = $this->actingAs($user)
+            ->put('http://localhost:8000/api/v1/addresses/4',$form_data)
+            ->assertStatus(200);
+    }
+
+    public function test_delete_address()
+    {   
+        $user = User::factory()->create();
+
+        $req = $this->actingAs($user)
+            ->delete('http://localhost:8000/api/v1/addresses/6')
+            ->assertStatus(200)
+            ->dump();
+    }
+
+    public function test_search_address()
+    {   
+        $user = User::factory()->create();
+
+        $req = $this->actingAs($user)
+            ->getJson('http://localhost:8000/api/v1/addresses/search/324')
+            ->assertStatus(200);
+    }
+
+    public function test_customers_address()
+    {   
+        $user = User::factory()->create();
+
+        $req = $this->actingAs($user)
+            ->get('http://localhost:8000/api/v1/addresses/customer/3')
+            ->assertStatus(200)
+            ->dump();
+    }
 }
